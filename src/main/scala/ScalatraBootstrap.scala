93805feff6d22451122f60546ce8278cda83b854
GOOD-Stuff <gutstuf@gmail.com>
2018-10-22 15:12:45 +0300

    Fix contents

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -5,13 +5,11 @@ import org.scalatra._
 
 class ScalatraBootstrap extends LifeCycle {
     override def init(context: ServletContext): Unit = {
-        println("Fuck you")
         DBWorker.connectDB()
         context.mount(new MyScalatraServlet, "/")
     }
 
     override def destroy(context:ServletContext) {
-        println("Good bye!")
         DBWorker.disconnectDB()
     }
 }
